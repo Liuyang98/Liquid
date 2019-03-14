fce85e9009e081e96578c5205ca2da0474f59c5d
Liuyang98 <449360145@qq.com>
2019-03-14 18:05:28 +0800

    接收传入布局的方式

--- sample/src/main/java/com/ly/demo/activity/NormalActivity.java
+++ sample/src/main/java/com/ly/demo/activity/NormalActivity.java
@@ -19,7 +19,6 @@ public class NormalActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
-
         init();
     }
 
